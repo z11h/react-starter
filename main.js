7dae8d037bdddcf5fcf4c64b6cef70d43baedd4f
Zakaria Ridouh <zridouh@live.com>
2016-06-13 19:14:33 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import App from './App';
+
+const app = document.getElementById('app')
+render(<App />, app);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333 // you can set this to any port you want
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+};
